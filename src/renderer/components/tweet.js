df8b4ab49d5e59459ecac5e0de181c9c2f68429d
akameco <akameco.t@gmail.com>
2016-02-15 03:38:01 +0900

    Update tweet.js

--- src/renderer/components/tweet.js
+++ src/renderer/components/tweet.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
-export default class Tweet extends React.Component {
-  render() {
-    const {screen_name, text, media_url, hovered} = this.props.data;
-    const display = this.props.hovered ? 'block' : 'none';
-    const divStyle = {display};
-
-    return (
-      <div>
-        <img src={media_url} />
-        <div className='tweet' style={divStyle}>
-          <p className='tweet__screen_name'>@{screen_name}</p>
-          <p className='tweet__text'>{text}</p>
-        </div>
+const Tweet = props => {
+  return (
+    <div>
+      <div className='tweet'>
+        <p className='tweet__screen_name'>@{props.data.screen_name}</p>
+        <p className='tweet__text'>{props.data.text}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Tweet;
